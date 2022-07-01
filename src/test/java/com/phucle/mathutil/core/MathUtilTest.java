74214560bb4ca6a979461536453b07b33b91cfbe
phuclh1406 <phuclh1406@gmail.com>
2022-07-01 09:34:19 +0700

    fix error

--- src/test/java/com/phucle/mathutil/core/MathUtilTest.java
+++ src/test/java/com/phucle/mathutil/core/MathUtilTest.java
@@ -4,33 +4,50 @@
  */
 package com.phucle.mathutil.core;
 
-import static com.phucle.mathutil.core.MathUtil.*;
-//import static la danh rieng cho nhung ham static
-//goi ham ma bo qua ten class
-import org.junit.jupiter.api.Test;
+import static com.phucle.mathutil.core.MathUtil.getFactorial;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
-/**
- *
- * @author predator
- */
 public class MathUtilTest {
 
-    @Test
-    public void testGetFactorialGivenRightArgReturnsWell() {
-        assertEquals(24, getFactorial(4));
+    //chơi DDT, tách data ra khỏi câu lệnh kiểm thử, tham số hóa cái data này
+    //vào trong câu lệnh kiểm thử
+    //chuẩn bị bộ data
+    public static Object[][] initDate() {
+        return new Integer[][]{
+            {1, 1},
+            {2, 2},
+            {3, 6},
+            {4, 24},
+            {5, 120},
+            {6, 720}
+        };
     }
 
-    //Bat ngoai le khi dua data ca chon
-    @Test
-    public void testGetFactorialGivenWrongArgThrowException() {
-        //xai bieu thuc lambda
-        //ham nhan tham so thu 2 la 1 cai object / code  co implement cai
-        //functional interface ten la Executable - co 1 ham duy nhat ko code
-        //ten la execute()
-        //choi cham
-        Executable excObject = new 
-        
-        assertThrows(IllegalArgumentException.class, executable);
+    @ParameterizedTest
+    @MethodSource(value = "initDate") //tên hàm cung cấp data, ngầm định thứ tự
+    //của các phần tử mảng, map vào tham số hàm
+    public void testGetFactorialGivenRightArgReturnsWell(int input, long expected) {
+        assertEquals(expected, getFactorial(input));
     }
+
+//   @Test
+//    public void testGetFactorialGivenWringArgThrowException() {
+        //xài biểu thức Lambda
+        //hàm nhận tham số thứ 2 là 1 cái object/có code implement cái
+        //functionl interface tên là Executable - có 1 hàm d nuyhất ko code
+        //têm là execte()
+        //chơi chậm
+//        Executable exObject = new Executable() {
+//            @Override
+//            public void execute() throws Throwable {
+//                getFactorial(-5);
+//            }
+//        };
+
+//        Executable exObject = () -> getFactorial(-5);
+//        assertThrows(IllegalArgumentException.class, () -> getFactorial(-5));
+//    }
+//
 }
